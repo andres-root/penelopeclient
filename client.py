72b3af4bd5397de85aaa99e63503b945c1a0adda
Karolina Ladino <ladinokarolina@gmail.com>
2018-08-19 00:30:40 -0500

    WIP

--- client.py
+++ client.py
@@ -12,13 +12,32 @@ from unidecode import unidecode
 
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
-# TODO: Add URL to env variables
 url = os.environ['URL']
 welcome_message = os.environ['WELCOME_MESSAGE']
 
 # Initialize printer
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
+# Print welcome message
+printer.print(unidecode(
+        HTMLParser.HTMLParser().unescape(welcome_message)
+    )
+)
 
+printer.feed(4)
+
+printer.print(unidecode(
+        HTMLParser.HTMLParser().unescape('Adriana Marmorek')
+    )
+)
+
+printer.feed(4)
+
+IO.setmode(IO.BOARD)
+IO.setup(29, IO.OUT)
+IO.output(29, True)
+time.sleep(2)
+IO.output(29, False)
+IO.cleanup()
 
 def setup():
     # LED configuration
@@ -27,17 +46,10 @@ def setup():
     IO.setup(31, IO.OUT)
     IO.setup(29, IO.OUT)
 
-    # Print welcome message
-    printer.print(unidecode(
-            HTMLParser.HTMLParser().unescape(welcome_message)
-        )
-    )
-
-
 def get_tweet():
     IO.output(33, True)
     time.sleep(1)
-    tweet = json.loads(requests.get(url))
+    tweet = json.loads(requests.get(url).text)
     IO.output(33, False)
     return tweet
 
@@ -52,7 +64,7 @@ def start_printing(tweet):
 
     # Start printing
     printer.inverseOn()
-    printer.print(' ' + '{:<31}'.format(tweet['user_name']))
+    printer.print(' ' + '{:<31}'.format(tweet['user']))
     printer.inverseOff()
 
     printer.underlineOn()
@@ -61,12 +73,13 @@ def start_printing(tweet):
 
     # Remove HTML escape sequences
     # and remap Unicode values to nearest ASCII equivalents
+    print(tweet['text'])
     printer.print(unidecode(
             HTMLParser.HTMLParser().unescape(tweet['text'])
         )
     )
 
-    printer.feed(3)
+    printer.feed(5)
 
     # Printer LED off
     IO.output(31, False)
@@ -74,7 +87,7 @@ def start_printing(tweet):
     time.sleep(5)
 
     IO.output(29, True)
-    time.sleep(3)
+    time.sleep(2)
     IO.output(29, False)
 
     IO.cleanup()
@@ -82,6 +95,4 @@ def start_printing(tweet):
 while True:
     setup()
     tweet = get_tweet()
-    print(tweet)
-    # start_printing(tweet)
-    # IO.cleanup()
+    start_printing(tweet)
